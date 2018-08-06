bffc8409f2e5428ce9651b3476ced7ba6f817353
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-08-06 15:35:33 -0400

    Rename test helper for accuracy

--- tests/test_dataframer.py
+++ tests/test_dataframer.py
@@ -18,7 +18,7 @@ class TestDataFrames(unittest.TestCase):
         self.assertEqual(a.index.tolist(), b.index.tolist(), message)
 
 
-class TestOptions(TestDataFrames):
+class TestKwargs(TestDataFrames):
     pass
 
 
@@ -31,8 +31,8 @@ class TestFileTypes(TestDataFrames):
             index=[1]
         )
 
-    def assert_file_read(self, input_bytes, df_target, label_map_target=None,
-                         kwargs={}, message=None):
+    def assert_good_parse(self, input_bytes, df_target, label_map_target=None,
+                          kwargs={}, message=None):
         stream = BytesIO(input_bytes)
         df_info = parse(stream, **kwargs)
         self.assertEqualDataFrames(df_info.data_frame, df_target, message)
@@ -41,7 +41,7 @@ class TestFileTypes(TestDataFrames):
 
     def test_read_crazy_delimiters(self):
         for c in '~!@#$%^&*|:;':
-            self.assert_file_read(
+            self.assert_good_parse(
                 bytes('{0}b{0}c\n1{0}2{0}3'.format(c), 'utf-8'),
                 self.target,
                 message='Failed with {} as delimiter'.format(c)
@@ -53,17 +53,17 @@ class TestFileTypes(TestDataFrames):
     #   >>> open('fake.csv.gz', 'rb').read()
 
     def test_read_gzip(self):
-        self.assert_file_read(
+        self.assert_good_parse(
             b'\x1f\x8b\x08\x08\xe5\xf2\x82Z\x00\x03fake.csv\x00\xd3I\xd2I\xe62\xd41\xd21\x06\x00\xfb\x9a\xc9\xa6\n\x00\x00\x00', self.target)  # noqa: E501
 
     def test_read_csv(self):
-        self.assert_file_read(b',b,c\n1,2,3', self.target)
+        self.assert_good_parse(b',b,c\n1,2,3', self.target)
 
     def test_read_csv_remove_strings(self):
-        self.assert_file_read(b',b,c,xxx\n1,2,3,X!', self.target)
+        self.assert_good_parse(b',b,c,xxx\n1,2,3,X!', self.target)
 
     def test_read_csv_keep_strings(self):
-        self.assert_file_read(
+        self.assert_good_parse(
             b',b,c,xxx\n1,2,3,X!',
             pandas.DataFrame([
                 [2, 3, 'X!']],
@@ -73,7 +73,7 @@ class TestFileTypes(TestDataFrames):
             kwargs={'keep_strings': True})
 
     def test_read_csv_merge_strings(self):
-        self.assert_file_read(
+        self.assert_good_parse(
             b',b,c,xxx,yyy\n1,2,3,X!,Y!',
             pandas.DataFrame([
                 [2, 3]],
@@ -85,18 +85,18 @@ class TestFileTypes(TestDataFrames):
             kwargs={'relabel': True})
 
     def test_read_csv_rn(self):
-        self.assert_file_read(b',b,c\r\n1,2,3', self.target)
+        self.assert_good_parse(b',b,c\r\n1,2,3', self.target)
 
     def test_read_csv_quoted(self):
-        self.assert_file_read(b',"b","c"\n"1","2","3"', self.target)
+        self.assert_good_parse(b',"b","c"\n"1","2","3"', self.target)
 
     def test_read_tsv(self):
-        self.assert_file_read(b'\tb\tc\n1\t2\t3', self.target)
+        self.assert_good_parse(b'\tb\tc\n1\t2\t3', self.target)
 
     def test_read_gct(self):
-        self.assert_file_read(
+        self.assert_good_parse(
             b'#1.2\n1\t1\nNames\tDescription\tb\tc\n1\tfoo\t2\t3', self.target)
 
     def test_read_gct_gz(self):
-        self.assert_file_read(
+        self.assert_good_parse(
             b'\x1f\x8b\x08\x085\xa3c[\x00\x03fake.gct\x00S6\xd43\xe22\xe44\xe4\xf2K\xccM-\xe6tI-N.\xca,(\xc9\xcc\xcf\xe3L\xe2L\x06\xca\xa4\xe5\xe7s\x1aq\x1a\x03\x00\xe7\xcc\xe5\xe5(\x00\x00\x00', self.target)  # noqa: E501
